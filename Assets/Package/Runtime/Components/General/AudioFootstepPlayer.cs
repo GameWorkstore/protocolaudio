c8f9d5d9de764b054462b5d2ae6829e9cbccd5c4
Hanza Ru <john.rosa@playkids.com>
2021-03-26 11:24:57 -0300

    +Initial commit

---
+++ Assets/Package/Runtime/Components/General/AudioFootstepPlayer.cs
@@ -0,0 +1,35 @@
+﻿using GameWorkstore.Patterns;
+using UnityEngine;
+
+namespace GameWorkstore.ProtocolAudio
+{
+    public class AudioFootstepPlayer : MonoBehaviour
+    {
+        public string AudioName;
+        protected int _audioNameHash;
+        protected AudioService _audioService;
+        public bool Bypass;
+        public float MinimumAmountOfTimeBetweenCalls = 0.1f;
+
+        private float _lastCall;
+
+        private void Awake()
+        {
+            Preprocess();
+        }
+
+        public void Preprocess()
+        {
+            _audioNameHash = Animator.StringToHash(AudioName);
+            _audioService = ServiceProvider.GetService<AudioService>();
+        }
+
+        public void PlayFootstep()
+        {
+            if (Bypass) return;
+            if (Time.realtimeSinceStartup < _lastCall + MinimumAmountOfTimeBetweenCalls) return;
+            _lastCall = Time.realtimeSinceStartup;
+            _audioService.Play3D(_audioNameHash, transform.position);
+        }
+    }
+}
---
+++ Assets/Package/Runtime/Components/General/AudioOnEnablePlayer.cs
@@ -0,0 +1,10 @@
+﻿namespace GameWorkstore.ProtocolAudio
+{
+    public class AudioOnEnablePlayer : AudioPlayer
+    {
+        private void OnEnable()
+        {
+            _audioService.Play2D(_audioNameHash);
+        }
+    }
+}
