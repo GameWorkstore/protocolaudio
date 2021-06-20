a78b197f768096ebc698b702d37f2401d4a921d9
Hanza Ru <john.rosa@playkids.com>
2021-06-20 12:39:58 -0300

    +Now protocol audio automatically collects all samples from children instead of list manually.

---
+++ Runtime/Core/AudioMaster.cs
@@ -0,0 +1,16 @@
+﻿using GameWorkstore.Patterns;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace GameWorkstore.ProtocolAudio
+{
+    public class AudioMaster : MonoBehaviour
+    {
+        public AudioMixer Master;
+
+        private void Awake()
+        {
+            ServiceProvider.GetService<AudioService>().SetCurrentMaster(Master);
+        }
+    }
+}
---
+++ Runtime/Core/AudioService.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using GameWorkstore.Patterns;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace GameWorkstore.ProtocolAudio
+{
+    public class AudioService : IService
+    {
+        private AudioMixer _master;
+        private readonly Dictionary<int, AudioPack> _audioDictionary = new Dictionary<int, AudioPack>();
+
+        public override void Preprocess()
+        {
+        }
+
+        public override void Postprocess()
+        {
+        }
+
+        internal void RegisterAudioPack(AudioPack audioPack)
+        {
+            int hash = Animator.StringToHash(audioPack.name);
+            if (!_audioDictionary.ContainsKey(hash))
+            {
+                _audioDictionary.Add(hash, audioPack);
+            }
+        }
+
+        internal void UnregisterAudioPack(AudioPack audioPack)
+        {
+            int hash = Animator.StringToHash(audioPack.name);
+            if (_audioDictionary.ContainsKey(hash))
+            {
+                _audioDictionary.Remove(hash);
+            }
+        }
+
+        internal void SetCurrentMaster(AudioMixer master)
+        {
+            _master = master;
+        }
+
+        public AudioSource Play2D(int audioNameHash)
+        {
+            if (_audioDictionary.TryGetValue(audioNameHash, out AudioPack pack))
+                return pack.Play2D();
+            return null;
+        }
+
+        public AudioSource Play3D(int audioNameHash, Vector3 position)
+        {
+            if (_audioDictionary.TryGetValue(audioNameHash, out AudioPack pack))
+                return pack.Play3D(position);
+            return null;
+        }
+
+        public AudioMixer GetCurrentMaster()
+        {
+            return _master;
+        }
+    }
+}
