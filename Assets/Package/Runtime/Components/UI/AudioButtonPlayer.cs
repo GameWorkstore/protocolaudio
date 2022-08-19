cf69402c38eade883e6e265b67bf10080621a1ba
John Willy de Souza Rosa <woodimbr@gmail.com>
2022-08-19 16:46:03 +0200

    +Removed for good

--- Assets/Package/Runtime/Components/UI/AudioButtonPlayer.cs
+++ Assets/Package/Runtime/Components/UI/AudioButtonPlayer.cs
@@ -4,16 +4,12 @@ using UnityEngine.EventSystems;
 
 namespace GameWorkstore.ProtocolAudio
 {
-    public class AudioButtonPlayer : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, ISubmitHandler, ISelectHandler
+    public class AudioButtonPlayer : MonoBehaviour, IPointerClickHandler, ISubmitHandler, ISelectHandler
     {
         [Tooltip("OnSelection of the element - Better for Console Games.")]
         public AudioName AudioNameSelected;
         [Tooltip("OnClicked - After Release button.")]
         public AudioName AudioNameClicked;
-
-        [Header("Advanced")]
-        [Tooltip("OnPressed - We cannot distinguish between click or drag.")]
-        public AudioName AudioNamePressed;
 
         private AudioService _audioService;
 
@@ -25,11 +21,6 @@ namespace GameWorkstore.ProtocolAudio
         void ISelectHandler.OnSelect(BaseEventData eventData)
         {
             if (AudioNameSelected) _audioService.Play2D(AudioNameSelected);
-        }
-
-        void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
-        {
-            if (AudioNamePressed) _audioService.Play2D(AudioNamePressed);
         }
 
         void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
